7eba161db42fe833d7af907995a0648eae25e236
Souax <xxsouaxx@gmail.com>
2024-07-21 22:27:55 +0900

    Firstcommit

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Sidebar.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="./logo192.png" alt="" />
+        </div>
+      </div>
+      {/* sidebarLeft */}
+      <div className="sidebarRight">
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
